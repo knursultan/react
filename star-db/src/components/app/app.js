12a5362cccd807f3aea790cb18247efd20c119e8
knursultan <knursultana@gmail.com>
2019-08-26 21:36:50 +0600

    star db

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -4,13 +4,18 @@ import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
+import PersonPage from "../people-page";
 
 import "./app.css";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5
+    selectedPerson: 2,
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
@@ -21,6 +26,10 @@ export default class App extends Component {
     });
   };
 
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
   onPersonSelected = id => {
     this.setState({
       selectedPerson: id
@@ -28,25 +37,24 @@ export default class App extends Component {
   };
 
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
     return (
-      <div>
+      <div className="stardb-app">
         <Header />
         {planet}
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}
-        >
-          Toggle Random Planet
-        </button>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}
+          >
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
         </div>
+        <PeoplePage />
       </div>
     );
   }
--- star-db/src/components/error-indicator/error-indicator.js
+++ star-db/src/components/error-indicator/error-indicator.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./error-indicator.css";
+import icon from "./death-star.png";
 
 const ErrorIndicator = () => {
   return (
