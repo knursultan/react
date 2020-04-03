c26437074ccd49ec5f123409268cb763494c2547
knursultan <knursultana@gmail.com>
2020-04-03 22:26:44 +0500

    74

--- star-db/src/components/item-list/item-list.js
+++ star-db/src/components/item-list/item-list.js
@@ -20,14 +20,17 @@ export default class ItemList extends Component {
   }
 
   renderItems(arr) {
-    return arr.map(({ id, name }) => {
+    return arr.map((item) => {
+      const { id } = item;
+      const label = this.props.renderItem(item);
+      console.log(this.props);
       return (
         <li
           className="list-group-item"
           key={id}
           onClick={() => this.props.onItemSelected(id)}
         >
-          {name}
+          {label}
         </li>
       );
     });
